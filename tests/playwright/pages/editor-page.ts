69eb505edf3297fd23f2d8c36dd7f455449e87e6
Hein van Vlastuin <hein@obox.co.za>
2025-08-05 12:25:01 +0200

    wip

--- tests/playwright/pages/editor-page.ts
+++ tests/playwright/pages/editor-page.ts
@@ -1403,6 +1403,7 @@ export default class EditorPage extends BasePage {
 		await this.page.locator( '#elementor-template-library-header-import i' ).click();
 		await this.page.getByText( 'Select File' ).click();
 		await this.page.locator( EditorSelectors.media.imageInp ).setInputFiles( filePath );
+		await this.page.getByRole( 'button', { name: 'Continue' } ).click();
 		await this.page.getByRole( 'button', { name: 'Insert' } ).first().click();
 	}
 
